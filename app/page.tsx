a5c18a0553dd100781e3b0875fa1378c2ae769dd
Macbook Tion <nechaev.of.work@gmail.com>
2026-05-03 16:41:36 +0300

    Finalize launch landing composition

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-[100svh] flex-col overflow-hidden bg-white px-6 pb-10 pt-24 text-black sm:pt-28">
+    <main className="relative flex min-h-[100svh] flex-col overflow-hidden bg-white px-6 pb-10 pt-32 text-black sm:pt-36">
       <header className="flex justify-center">
-        <div className="text-[34px] font-normal leading-none tracking-[0] sm:text-[42px]">
+        <div className="text-[40px] font-normal leading-none tracking-[0] sm:text-[50px]">
           <span>Мусор </span>
           <span className="font-black text-[#9edca5]">ОК</span>
         </div>
@@ -29,7 +29,16 @@ export default function Home() {
             width={1024}
             height={1536}
             priority
-            className="pointer-events-none relative z-20 mx-auto mt-8 h-auto w-[180px] select-none drop-shadow-[0_18px_28px_rgba(0,0,0,0.14)] min-[520px]:absolute min-[520px]:left-[calc(50%+104px)] min-[520px]:top-1/2 min-[520px]:mt-0 min-[520px]:w-[170px] min-[520px]:-translate-y-[36%] sm:left-[calc(50%+150px)] sm:w-[220px] md:left-[calc(50%+190px)] md:w-[260px] lg:left-[calc(50%+225px)] lg:w-[310px]"
+            className="pointer-events-none relative z-20 mx-auto mt-8 h-auto w-[180px] select-none drop-shadow-[0_18px_28px_rgba(0,0,0,0.14)] min-[520px]:absolute min-[520px]:left-[calc(50%+86px)] min-[520px]:top-1/2 min-[520px]:mt-0 min-[520px]:w-[170px] min-[520px]:-translate-y-[28%] sm:left-[calc(50%+128px)] sm:w-[220px] md:left-[calc(50%+168px)] md:w-[260px] lg:left-[calc(50%+198px)] lg:w-[310px]"
+          />
+
+          <Image
+            src="/trash-bag.png"
+            alt=""
+            width={728}
+            height={880}
+            priority
+            className="pointer-events-none absolute left-[calc(50%+146px)] top-1/2 z-10 h-auto w-[58px] translate-y-[95%] select-none drop-shadow-[0_12px_18px_rgba(0,0,0,0.12)] min-[520px]:left-[calc(50%+216px)] min-[520px]:w-[62px] sm:left-[calc(50%+280px)] sm:w-[76px] md:left-[calc(50%+336px)] md:w-[88px] lg:left-[calc(50%+408px)] lg:w-[108px]"
           />
         </div>
       </section>
